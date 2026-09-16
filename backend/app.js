ff9b851f7c556ecc3350b4a58322829457db45aa
UMAIR QASIM <umairqasim110@gmail.com>
2026-09-16 15:26:11 +0500

    feat: add the .env files and setup the folder structure.

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,20 @@
 import express from "express";
 import cors from "cors";
+import { corsOptions } from "./config/corsOptions.js";
 
 const app = express();
 
-const allowedOrigins = ["http://localhost:5173", "https://mywebsite.com"];
 
+// Middlewares
 app.use(express.json());
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    optionsSuccessStatus: 200,
-  }),
-);
+app.use(cors(corsOptions));
 
+
+// Routes
 app.get("/", (req, res) => {
   res.json({ message: "Hello" });
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port http://localhost:3000/");
-});
+
+
+export default app
---
+++ backend/config/corsOptions.js
@@ -0,0 +1,13 @@
+const allowedOrigins = ["http://localhost:5173", "https://mywebsite.com"];
+
+export const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
