3aa7aed9acdafae57d5aa2e0295a2e9ce485fd6c
UMAIR QASIM <umairqasim110@gmail.com>
2026-09-24 15:47:52 +0500

    fix: cors issue on production.

--- backend/app.js
+++ backend/app.js
@@ -7,13 +7,20 @@ import pageRoutes from "./routes/page.routes.js";
 const app = express();
 
 // Middlewares
-app.use(express.json());
 app.use(cors(corsOptions));
+app.use(express.json());
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1", pageRoutes);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Route not found" });
+});
+
+app.use((err, req, res, next) => {
+  console.error("Server Error:", err.message);
+  res.status(err.status || 500).json({
+    message: err.message || "Internal Server Error",
+  });
 });
 
 export default app;
